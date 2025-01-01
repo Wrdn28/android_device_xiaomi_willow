940f5bf250cc8f31675b975ae4b6647108fa56fb
Michael Bestas <mkbestas@lineageos.org>
2025-01-02 01:12:07 +0200

    ginkgo: Disable OMX service
    Change-Id: I5381e098af64271f9d71c484262e1710e8fcacea
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -17,6 +17,7 @@
 
 # Inherit framework first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
