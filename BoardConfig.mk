53f4b632df6a4d6a417cf533f6bf8a82602f23a6
Skyblueborb <tomaszborbely0710@gmail.com>
2025-01-25 12:30:29 +0100

    ginkgo: media: Import Codec2 from sweet
    sm6150 and sm6125 share the same encoders/decoders, meaning we can
    migrate to C2 as well.
    
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,7 @@ TARGET_USES_HARDWARE_QCOM_GPS := false
 USE_OPENGL_RENDERER := true
 
 # HIDL
+DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
